28fd607b1a79bb689da4ba042d322126ac076197
dane-layton <lay15007@byui.edu>
2017-06-07 19:51:15 -0600

    stylesheet

--- javascript/html5Demo.js
+++
@@ -1,8 +0,0 @@
-function draw() {
-  var c = document.getElementById("mycanvas");
-  var ctx = c.getContext("2d");
-  var textBox = document.getElementById("myText");
-  var text = textBox.value;
-  ctx.font = "30px Arial";
-  ctx.fillText(text,10,30);
-}
